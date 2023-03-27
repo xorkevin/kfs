a40a4533051dcd7cc6d2ca5f5579d452e3dd49f0
Kevin Wang <kevin@xorkevin.com>
2023-03-26 21:01:22 -0700

    Add writefs test

--- writefs/writefs_test.go
+++ writefs/writefs_test.go
@@ -1,1 +1,53 @@
-package writefs
+package writefs_test
+
+import (
+	"io/fs"
+	"os"
+	"testing"
+	"testing/fstest"
+
+	"github.com/stretchr/testify/require"
+	"xorkevin.dev/kfs/kfstest"
+	"xorkevin.dev/kfs/writefs"
+)
+
+func Test_WriteFS(t *testing.T) {
+	t.Parallel()
+
+	assert := require.New(t)
+
+	tempDir := t.TempDir()
+
+	testFiles := []kfstest.TestFSFile{
+		{
+			Name: "foo.txt",
+			Data: []byte("hello, world"),
+		},
+		{
+			Name: "bar/foobar.txt",
+			Data: []byte("foo bar"),
+		},
+		{
+			Name: "other/subother/subother.txt",
+			Data: []byte("subother"),
+		},
+	}
+
+	fsys := writefs.New(os.DirFS(tempDir), tempDir)
+
+	assert.NoError(writefs.WriteFile(fsys, "foo.txt", []byte("hello, world"), 0o644))
+	assert.NoError(kfstest.TestFileWrite(fsys, "bar/foobar.txt", []byte("foo bar")))
+	subFsys, err := fs.Sub(fsys, "other")
+	assert.NoError(err)
+	assert.NoError(kfstest.TestFileWrite(subFsys, "subother/subother.txt", []byte("subother")))
+
+	fileNames := make([]string, 0, len(testFiles))
+	for _, i := range testFiles {
+		fileNames = append(fileNames, i.Name)
+	}
+	assert.NoError(fstest.TestFS(fsys, fileNames...))
+
+	assert.NoError(kfstest.TestFS(fsys, testFiles...))
+
+	assert.NoError(kfstest.TestFileAppend(subFsys, "subother/subother.txt", []byte("more")))
+}
